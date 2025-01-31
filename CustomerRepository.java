c1ea0259f46966d3bdf6cbed609d3adcd4a11a0c
BYUUSAA <byuussaa@gmail.com>
2025-01-31 01:18:22 +0000

    Updates

--- CustomerRepository.java
+++
@@ -1,5 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, String> {
-    // Custom methods for customer retrieval can go here if needed
-}
